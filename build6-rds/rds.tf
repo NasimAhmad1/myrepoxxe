7f55ba97e98b7e6f19245cabaff7ad2e36f5a07b
nasim103 <nasim1039@gmail.com>
2024-03-04 19:59:46 +0530

    subnet

--- build6-rds/rds.tf
+++ build6-rds/rds.tf
@@ -27,7 +27,7 @@ resource "aws_db_instance" "maridb_instace" {
   identifier = "mariadb"
   username = "root"
   password = "mariadb123"
-  db_subnet_group_name = aws_db_subnet_group.Mariadb_private.name
+  db_subnet_group_name = aws_db_subnet_group.mariadb_private.name
   parameter_group_name = aws_db_parameter_group.mariabd-parameter.name
   multi_az = "false"
   vpc_security_group_ids = [aws_security_group.allow_mariadb.id]
