868148ab81f3a3622c8c0d554db4f4f595e3a09a
nasim103 <nasim1039@gmail.com>
2024-03-12 23:09:07 +0530

    ami

--- case-studey-1/webserver/variable.tf
+++ case-studey-1/webserver/variable.tf
@@ -33,7 +33,7 @@ variable "SSH_CIDR_WEB_SERVER" {
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
+        us-east-1 = "ami-0f5daaa3a7fb3378b"
         us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-02c8896b265d8c480"
         eu-west-1 = "ami-0cdd3aca00188622e"
