8e6e3e2520737402150c2e1fb45ad5107c5c90d3
nasim103 <nasim1039@gmail.com>
2024-08-24 12:09:48 +0530

    key1

--- udemy-terraform/Basic-Terraform/provider.tf
+++ udemy-terraform/Basic-Terraform/provider.tf
@@ -17,8 +17,8 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
-  access_key = AWS_ACCESS_KEY
-  secret_key = AWS_SECRET_KEY
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
   default_tags  {
     tags = {
       BuildBy = "Terraform"
