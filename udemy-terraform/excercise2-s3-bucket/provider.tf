c5158bad9aa37a7ac73e49fb8af5ec97e0a87361
nasim103 <nasim1039@gmail.com>
2024-08-24 16:49:06 +0530

    bucket

---
+++ udemy-terraform/excercise2-s3-bucket/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = "~>1.3"
+
+  backend "s3" {
+    bucket = "terraform-project-r1"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+    dynamodb_table = "terraform-state-table"
+    
+  }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.4"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  default_tags  {
+    tags = {
+      BuildBy = "Terraform"
+      MaintainedBy = "Nasim Ahmad"
+      project = "Basic terraform"
+    }
+    
+  }
+}
+
