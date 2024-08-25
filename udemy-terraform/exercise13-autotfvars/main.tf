49472296fd6e7d867c03654303e86a3f34c2fec9
nasim103 <nasim1039@gmail.com>
2024-08-25 23:56:08 +0530

    datasource

--- udemy-terraform/exercise13-autotfvars/main.tf
+++ udemy-terraform/exercise13-autotfvars/main.tf
@@ -3,8 +3,18 @@ resource "random_id" "bucket_suffix" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "terraform-autotfvars-${random_id.bucket_suffix.decimal}"
+  bucket = "terraform-autotfvars-${random_id.bucket_suffix.dec}"
   tags = merge(local.tags, { 
   SupportBy = "Nasim"
  })
+}
+
+data "aws_region" "region" {}
+
+output "aws_region" {
+  value = data.aws_region.region.name
+}
+
+output "s3-bucket" {
+  value = aws_s3_bucket.bucket.bucket
 }
